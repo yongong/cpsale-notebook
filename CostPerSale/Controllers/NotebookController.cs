6260216cd4df423342ededa4b1e6a6ab98095801
woyo <761079107@qq.com>
2018-04-30 14:25:04 +0800

    初建项目

---
+++ CostPerSale/Controllers/NotebookController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CostPerSale.Controllers
+{
+    public class NotebookController : Controller
+    {
+        public ActionResult VideoGame()
+        {
+            return View();
+        }
+
+        public ActionResult BusinessOffice()
+        {
+            return View();
+        }
+
+        public ActionResult Hot()
+        {
+            return View();
+        }
+    }
+}
---
+++ CostPerSale/Controllers/testApiController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Routing;
+
+namespace CostPerSale.Controllers
+{
+    public class testApiController : ApiController
+    {
+        protected void Application_Start()
+        {
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+        }
+
+        // GET: api/test
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/test/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/test
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/test/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/test/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
